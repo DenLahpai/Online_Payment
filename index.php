61668e263fd5f166b7c62955d77a9aa5e9fd60dd
Den Lahpai <den@devserver>
2018-11-05 18:37:38 +0630

    starting

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+require "../conn.php";
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <?php
+    $page_title = "Welcome";
+    include "includes/head.html";
+    ?>
+    <body>
+
+    </body>
+</html>
